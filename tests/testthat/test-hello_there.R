e44b8208a92325520c5ef1ccfa87456b3b34f26a
Eckart Bindewald <bindewald@gmail.com>
2018-10-30 08:42:42 -0400

    added tests and one additional context

--- tests/testthat/test-hello_there.R
+++ tests/testthat/test-hello_there.R
@@ -7,3 +7,19 @@ test_that("multiplication works", {
 test_that("hello_there works for simple example", {
           expect_equal(hello_there("Susan"), "Hello Susan!")
 })
+
+test_that("hello_there meets simple expectations for simple example", {
+          result <- demoRpackage::hello_there("Frank")
+          expect_is(result, "character") # several expectations are tested
+	  expect_equal(length(result), 1)
+})
+
+# group advanced test in second context:
+context("test-hello_there:advanced")
+
+test_that("hello_there returns NA for NA", {
+          testthat::skip("function hello_there: NA not yet implemented") # skip test - functionality is not yet implemented
+          result <- demoRpackage::hello_there(NA)
+          expect_true(result, is.na(NA))
+})
+
