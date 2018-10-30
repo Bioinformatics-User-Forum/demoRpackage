0093efe10755af57ef3058348b25b8c901ed985a
Eckart Bindewald <bindewald@gmail.com>
2018-10-30 08:42:13 -0400

    added comments

--- R/hello_there.R
+++ R/hello_there.R
@@ -1,11 +1,19 @@
-#' print welcome message
+#' Print Welcome Message
 #' 
+#' This is a simple function to demonstrate the testing framework
+#' as as as the documentation.
+#' Test with: `devtools::test()`
+#' Create new test with: `devtools::use_test("hello_there_my_new_nest")`
+#' Update documentation with: `devtools::document()`
+#' Install updates: `devtools::install()`
+#'
+#' @name hello_there
 #' @param name A character string
 #' @return A character string containing a greeting
 #' @examples 
 #' hello_there("Susan")
 #' @export
-hello_there <- function(name)
-{
+hello_there <- function(name) {
     s <- paste0("Hello ", name, "!")
+    s
 }
